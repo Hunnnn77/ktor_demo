779ff6e9a6ed2d506c1063bdd0ce83a5bc57a086
hun <k01084758975dev@gmail.com>
2026-01-22 10:20:03 +0900

    init

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,26 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(val name: String)
+
+fun Application.configureRouting() {
+    routing {
+        post("/") {
+            val user = call.receive<User>()
+        }
+
+        get("/") {
+            val user = User(
+                name = "hello owlrd"
+            )
+            val (name) = user
+            call.respondText("Hello World!")
+        }
+    }
+}
